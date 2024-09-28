8aaa3073a51928ce73160e1a4eacfcfa400baee2
SJM <1206127263@qq.com>
2024-09-28 12:25:18 +0800

    xargs

--- user/xargs.c
+++ user/xargs.c
@@ -3,6 +3,7 @@
 #include "user/user.h"
 
 #define buf_size 512
+#define MSGSIZE 16
 // echo hello too | xargs echo bye
 // bye hello too
 /*
@@ -17,6 +18,41 @@ int main(int argc, char *argv[])
     read(0, buf, buf_size);
     printf("标准化输入是：%s\n", buf);
     // 2：如何获得到自己的命令行参数 argv里面
+    char *xargv[MSGSIZE];
+    int xargc = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        xargv[xargc] = argv[i];
+        xargc++;
+    }
+    // 以换行符进行切割
+    char *p = buf; // 前面拿到的标准输出
+    for (int i = 0; i < MSGSIZE; ++i)
+    {
+        if (buf[i] == '\n')
+        {
+            int pid = fork();
+            if (pid > 0)
+            {
+                p = &buf[i + 1];
+                wait(0);
+            }
+            else
+            {
+                // 3：如何使用exec去执行命令
+                buf[i] = 0;
+                xargv[xargc] = p;
+                xargc++;
+                xargv[xargc] = 0;
+                xargc++;
+                exec(xargv[0], xargv);
+                exit(0);
+            }
+        }
+    }
+
     // 3：如何使用exec去执行命令
+    // exec("echo", argv);
+    wait(0);
     exit(0);
 }
